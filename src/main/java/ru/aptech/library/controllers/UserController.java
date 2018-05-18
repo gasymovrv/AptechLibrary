0a2a620904a8b830edebc2b7808920cd32ad9e6e
gasymovrv <04gas90@mail.ru>
2018-05-18 19:35:32 +0400

    add UserController

---
+++ src/main/java/ru/aptech/library/controllers/UserController.java
@@ -0,0 +1,31 @@
+package ru.aptech.library.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class UserController {
+
+	@RequestMapping(value = { "/", "/authorization" }, method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Неверное имя пользователя или пароль!");
+		}
+
+		model.setViewName("login-page");
+
+		return model;
+
+	}
+
+}
